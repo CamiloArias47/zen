3c03b1bbd3e7e96f1b44cb9b757f3fe1b4087887
CamiloArias47 <camillo47@gmail.com>
2019-07-30 23:11:47 -0500

    Resultado de juego solitario

--- src/views/components/playGame/resultGame.js
+++ src/views/components/playGame/resultGame.js
@@ -1,10 +1,40 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+
+class CardResult extends React.Component{
+
+    render(){
+        return(
+            <a href="#">
+                <div className="card">
+                    <div className="card-image">
+                        <h1>{this.props.result}</h1>
+                    </div>
+                    <div className="card-content">
+                        <h3>{this.props.title}</h3>
+                    </div>
+                </div>
+            </a>
+        )
+    }
+}
+
 
 class ResultGame extends React.Component{
 
     render(){
         return(
-            <h1>Resultados</h1>
+            <div className="row center-align">
+                <div className="col s12 m6">
+                    <CardResult result={this.props.results.right} title="Correctas"/>
+                </div>
+                <div className="col s12 m6">
+                    <CardResult result={this.props.results.wrong} title="Incorrectas"/>
+                </div>
+                <div className="col s12">
+                    <Link to="/game" className="waves-effect waves-light btn-large">Volver</Link>
+                </div>
+            </div>
         )
     }
 }
